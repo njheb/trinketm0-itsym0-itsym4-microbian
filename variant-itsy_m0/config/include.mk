cf0cb82ab46743727dbb2877e04cbdcf1d973faf
Nick Hebdon <nick@rigare.co.uk>
2022-05-14 20:48:56 +0100

    itsy_m0 for blink in place tackle itsy_m4 next then look at moving pattern out onto other x00-... directories

---
+++ variant-itsy_m0/config/include.mk
@@ -0,0 +1,9 @@
+#trinket_m0 and itsybitsy_m0
+INCLUDE += -I ~/.arduino15/packages/adafruit/tools/CMSIS-Atmel/1.2.2/CMSIS/Device/ATMEL \
+          -I ~/.arduino15/packages/adafruit/tools/CMSIS-Atmel/1.2.2/CMSIS/Device/ATMEL/samd21 \
+          -I ~/.arduino15/packages/adafruit/tools/CMSIS/5.4.0/CMSIS/Core/Include
+
+#          -I $(VARIANT_PATH) \
+#          -I ../microbian
+
+
