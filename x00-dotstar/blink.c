f98fdd5c1225dd45872d595aa4f2f78ee6506475
Nick Hebdon <nick@rigare.co.uk>
2022-05-21 16:22:04 +0100

    fix compiler error that crops up on pi

--- x00-dotstar/blink.c
+++ x00-dotstar/blink.c
@@ -88,7 +88,7 @@ int count = 100;
 #ifndef WITHOUT_SAM_H
 uint32_t port = 0;
 const uint32_t pin = PIN_REAL_LED;
-const uint32_t pinMask = (1ul << pin);
+const uint32_t pinMask = (1ul << PIN_REAL_LED);
 #endif
 
 void test_taskA(int arg)
